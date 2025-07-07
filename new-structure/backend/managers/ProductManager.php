ded2502463839ea4171886455fd68054d3e7ec49
parodez <166378157+parodez@users.noreply.github.com>
2025-07-07 12:05:12 +0800

    Refactor ProductManager methods for improved product handling and error management

--- new-structure/backend/managers/ProductManager.php
+++ new-structure/backend/managers/ProductManager.php
@@ -6,121 +6,101 @@ class ProductManager
     // private PDO $pdo;
 
     public function __construct(
-        private PDO $pdo,
-        private CacheManager $cache
+        private PDO $pdo
     ) {}
 
-    public function getByType($cacheKey): array
-    {
-        $cached = $this->cache->get($cacheKey);
-        if ($cached != false) return $cached;
-
-        $stmt = $this->pdo->query('SELECT * FROM tbl_' . $cacheKey);
-        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        $this->cache->set($cacheKey, $data, 300);
-        return $data;
-    }
-    public function deleteByTypeAndId(string $product, int $id): array
+    public function getProducts(): array
     {
         $success = true;
-        $message = 'Product Successfully Deleted';
+        $message = 'Products Retrieved Successfully';
+        $data = [];
 
         try {
-            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tbl_' . $product . ' WHERE ' . $product . '_id=?');
-            $stmt->execute([$id]);
-            $count = $stmt->fetchColumn();
+            $stmt = $this->pdo->query('SELECT COUNT(*) FROM tbl_products');
+            if ($stmt->fetchColumn() < 1) throw new Exception('No Products Found');
 
-            if ($count < 0) throw new Exception('Product does not exist');
-            else {
-                $stmt = $this->pdo->prepare('DELETE FROM tbl_' . $product . ' WHERE ' . $product . '_id=?');
-                $stmt->execute([$id]);
-                $updateResult = $this->updateCache($product);
-                if (!$updateResult['success']) throw new Exception('Product Deleted. Cache Update Unsuccessful: ' . $updateResult['message']);
+            $stmt = $this->pdo->query('SELECT * FROM tbl_products');
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            $success = false;
+            $message = $e->getMessage();
+        }
+
+        return ['success' => $success, 'message' => $message, 'data' => $data];
+    }
+    public function deleteById(array $data): array
+    {
+        $success = true;
+        $message = 'Product Deleted Successfully';
+
+        try {
+            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tbl_products WHERE products_id=?');
+            $stmt->execute([$data['id']]);
+            if ($stmt->fetchColumn() < 0) throw new Exception('Product does not exist');
+
+            $stmt = $this->pdo->prepare('DELETE FROM tbl_products WHERE products_id=?');
+            $stmt->execute([$data['id']]);
+
+            $updateResult = $this->updateCache('products');
+            if (!$updateResult['success']) throw new Exception('Product Deleted. Cache Update Unsuccessful: ' . $updateResult['message']);
+        } catch (Exception $e) {
+            $success = false;
+            $message = $e;
+        }
+        return ['success' => $success, 'message' => $message];
+    }
+    public function addProduct($data): array
+    {
+        $success = true;
+        $message = 'Product Added Successfully';
+
+        try {
+            if (!isset($data['item'], $data['desc'], $data['cost'], $data['srp'], $data['service'])) throw new Exception('Missing Fields');
+
+            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tbl_products WHERE products_id=?');
+            $stmt->execute([$data['item']]);
+            if ($stmt->fetchColumn() > 0) throw new Exception('Product Already Exists');
+
+            $stmt = $this->pdo->prepare('INSERT INTO tbl_products (products_item, products_description, products_cost, products_srp, services_id) VALUES (?, ?, ?, ?, ?)');
+            $stmt->execute([$data['item'], $data['desc'], $data['cost'], $data['srp'], $data['service']]);
+
+            $updateResult = $this->updateCache('products');
+            if (!$updateResult['success']) throw new Exception('Product Added. Cache Update Unsuccessful: ' . $updateResult['message']);
+        } catch (Exception $e) {
+            $success = false;
+            $message = $e->getMessage();
+        }
+
+        return ['success' => $success, 'message' => $message];
+    }
+    public function editProduct(array $data): array
+    {
+        // int $id, string $robots_item, string $robots_description, float $robots_cost, float $robots_srp
+        $success = true;
+        $message = 'Product Edited Successfully';
+
+        try {
+            if (!isset($data['id'])) throw new Exception('Missing Field');
+
+            $keys = [];
+            $values = [];
+            foreach ($data as $key => $value) {
+                if ($key != 'id') $keys[] = "$key =?";
+                $values[] = $value;
             }
+
+            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tbl_products WHERE products_id=?');
+            $stmt->execute([$data['id']]);
+            if ($stmt->fetchColumn() < 1) throw new Exception('Robot does not exist');
+
+            $stmt = $this->pdo->prepare('UPDATE tbl_products SET ' . implode(', ', $keys) . 'WHERE products_id=?');
+            $stmt->execute($values);
+
+            $data = $this->updateCache('products');
+            if (!$data['success']) throw new Exception($data['message']);
         } catch (Exception $e) {
             $success = false;
-            $message = $e;
-        }
-        return ['success' => $success, 'message' => $message];
-    }
-    public function addRobot(string $item, string $desc, float $cost, float $srp): array
-    {
-        $success = true;
-        $message = 'Robot Successfully Added';
-
-        try {
-            $stmt = $this->pdo->prepare('INSERT INTO tbl_robots (robots_item, robots_description, robots_cost, robots_srp) VALUES (?, ?, ?, ?)');
-            $stmt->execute([$item, $desc, $cost, $srp]);
-            $updateResult = $this->updateCache('robots');
-            if (!$updateResult['success']) throw new Exception('Robot Added. Cache Update Unsuccessful: ' . $updateResult['message']);
-        } catch (Exception $e) {
-            $success = false;
-            $message = $e;
-        }
-
-        return ['success' => $success, 'message' => $message];
-    }
-    public function editRobot(int $id, string $robots_item, string $robots_description, float $robots_cost, float $robots_srp): array
-    {
-        $success = true;
-        $message = 'Robot Successfully Edited';
-
-        try {
-            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tbl_robots WHERE robots_id=?');
-            $stmt->execute([$id]);
-            $count = $stmt->fetchColumn();
-
-            if ($count < 1) throw new Exception('Robot does not exist');
-            else {
-                $stmt = $this->pdo->prepare('UPDATE tbl_robots SET robots_item=?,robots_description=?, robots_cost=?, robots_srp=? WHERE robots_id=?');
-                $stmt->execute([$robots_item, $robots_description, $robots_cost, $robots_srp, $id]);
-                $data = $this->updateCache('robots');
-                if (!$data['success']) throw new Exception($data['message']);
-            }
-        } catch (Exception $e) {
-            $success = false;
-            $message = $e;
-        }
-
-        return ['success' => $success, 'message' => $message];
-    }
-    public function addService(string $type, float $cost)
-    {
-        $success = true;
-        $message = 'Service Successfully Added';
-
-        try {
-            $stmt = $this->pdo->prepare('INSERT INTO tbl_services (services_type, services_cost) VALUES (?, ?)');
-            $stmt->execute([$type, $cost]);
-            $updateResult = $this->updateCache('services');
-            if (!$updateResult['success']) throw new Exception('Service Added. Cache Update Unsuccessful: '  . $updateResult['message']);
-        } catch (Exception $e) {
-            $success = false;
-            $message = $e;
-        }
-
-        return ['success' => $success, 'message' => $message];
-    }
-    public function editService(int $id, string $type, float $cost): array
-    {
-        $success = true;
-        $message = 'Service Successfully Edited';
-
-        try {
-            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tbl_services WHERE services_id=?');
-            $stmt->execute([$id]);
-            $count = $stmt->fetchColumn();
-            if ($count < 1) throw new Exception('Service does not exist');
-            else {
-                $stmt = $this->pdo->prepare('UPDATE tbl_services SET services_type=?, services_cost=? WHERE services_id=?');
-                $stmt->execute([$type, $cost, $id]);
-                $data = $this->updateCache('services');
-                if (!$data['success']) throw new Exception($data['message']);
-            }
-        } catch (Exception $e) {
-            $success = false;
-            $message = $e;
+            $message = $e->getMessage();
         }
 
         return ['success' => $success, 'message' => $message];
@@ -134,7 +114,7 @@ class ProductManager
             $stmt = $this->pdo->query('SELECT * FROM tbl_' . $cacheKey);
             $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            $this->cache->set($cacheKey, $data, 300);
+            // $this->cache->set($cacheKey, $data, 300);
         } catch (Exception $e) {
             $success = false;
             $message = $e;
